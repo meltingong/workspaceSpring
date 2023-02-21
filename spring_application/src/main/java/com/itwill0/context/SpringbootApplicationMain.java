9ca09f5a9ea89093d18ef8c20e04453cdca42ee7
meltingong <hwk1210@naver.com>
2023-02-21 15:13:11 +0900

    ..

---
+++ spring_application/src/main/java/com/itwill0/context/SpringbootApplicationMain.java
@@ -0,0 +1,11 @@
+package com.itwill0.context;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringbootApplicationMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplicationMain.class, args);
+	}
+
+}
