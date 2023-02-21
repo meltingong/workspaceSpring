849fc6b9f3336f4b4651b0d88636867a8c652933
meltingong <hwk1210@naver.com>
2023-02-21 15:16:50 +0900

    ..

--- spring_application/src/main/java/com/itwill0/context/SpringbootApplicationMain.java
+++ spring_application/src/main/java/com/itwill0/context/SpringbootApplicationMain.java
@@ -1,11 +1,16 @@
 package com.itwill0.context;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+@SpringBootApplication
+@ComponentScan(basePackages = "com.itwill.product")
 public class SpringbootApplicationMain {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplicationMain.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(SpringbootApplicationMain.class, args);
 	}
 
 }
