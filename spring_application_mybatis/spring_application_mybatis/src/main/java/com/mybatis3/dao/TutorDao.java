a13196a2d8914bba626ec91550dcd33a78602dff
meltingong <hwk1210@naver.com>
2023-02-27 14:39:00 +0900

    ...

--- spring_application_mybatis/spring_application_mybatis/src/main/java/com/mybatis3/dao/TutorDao.java
+++ spring_application_mybatis/spring_application_mybatis/src/main/java/com/mybatis3/dao/TutorDao.java
@@ -1,5 +1,14 @@
 package com.mybatis3.dao;
 
-public class TutorDao {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import com.mybatis3.dao.mapper.TutorMapper;
+
+@Repository
+public class TutorDao {
+	@Autowired
+	private TutorMapper tutorMapper;
+	
+	
 }
