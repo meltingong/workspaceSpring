bc1e63305f8846ae1a43d0c1221c5bd412d1d612
meltingong <hwk1210@naver.com>
2023-03-03 11:04:00 +0900

    ..

--- user_spring/src/main/java/com/itwill/user/controller/UserController.java
+++ user_spring/src/main/java/com/itwill/user/controller/UserController.java
@@ -53,28 +53,48 @@ public class UserController {
 	@RequestMapping("/user_login_action")
 	public String user_login_action_post(HttpServletRequest request,@ModelAttribute User user) throws Exception {
 		HttpSession session = request.getSession();
-
+		String forwardPath = "";
 		User fuser= new User(user.getUserId(),user.getPassword(),"","");
 		int login = userService.login(user.getUserId(), user.getPassword());
 		if(login == 0) {
+			String msg1 = user.getUserId()+"는 존재하지 않는 아이디 입니다.";
+			request.setAttribute("msg1", msg1);
+			request.setAttribute("fuser", fuser);
+			forwardPath = "user_login_form";
+		}else if(login ==1) {
+			String msg2 = "아이디 또는 비밀번호가 일치하지 않습니다.";
+			request.setAttribute("msg2", msg2);
+			request.setAttribute("fuser", fuser);
+			forwardPath = "user_login_form";
+		}else {
+			session.setAttribute("sUserId", fuser.getUserId());
+			forwardPath = "redirect:user_main";
 			
 		}
 		
-		String forwardPath = "redirect:user_main";
 		return forwardPath;
 	}
 	
-
-	public String user_view() throws Exception {
+	@RequestMapping("/user_view")
+	public String user_view(HttpServletRequest request) throws Exception {
 		/************** login check **************/
+		HttpSession session = request.getSession();
+		String sUserId = (String)session.getAttribute("sUserId");
 		String forwardPath = "";
+		if(sUserId == null) {
+			forwardPath = "redirect:user_login_form";
+		}else {
+			User loginUser = userService.findUser(sUserId);
+			request.setAttribute("loginUser", loginUser);
+			forwardPath = "user_view";
+		}
 		return forwardPath;
 	}
 
-
-	public String user_modify_form_post() throws Exception {
+	@RequestMapping("/user_modify_form")
+	public String user_modify_form_post(HttpServletRequest request) throws Exception {
 		/************** login check **************/
-
+		
 		String forwardPath = "";
 
 		return forwardPath;
@@ -91,10 +111,19 @@ public class UserController {
 		String forwardPath = "";
 		return forwardPath;
 	}
-
-	public String user_logout_action(HttpSession session) {
+	
+	@RequestMapping("/user_logout_action")
+	public String user_logout_action(HttpServletRequest request) {
 		/************** login check **************/
+		HttpSession session = request.getSession();
 		String forwardPath = "";
+		String sUserId=(String)session.getAttribute("sUserId");
+		if(sUserId == null) {
+			forwardPath = "redirect:user_login_form";
+		}else {
+			session.invalidate();
+			forwardPath = "redirect:user_main";
+		}
 		return forwardPath;
 	}
 
