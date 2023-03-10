e94f49cd3075e2de94f8fed6b7d6bd7169f2db08
meltingong <hwk1210@naver.com>
2023-03-10 18:18:46 +0900

    ..

--- guest_javascript_ajax/WebContent/js/app.js
+++ guest_javascript_ajax/WebContent/js/app.js
@@ -42,7 +42,31 @@ document.addEventListener('click',function(e){
 	console.log('Event Target 객체 id:'+e.target.id);
 	console.log('Event Target 객체 id:'+e.target.getAttribute("id"));
 	console.log('Event Target 객체 className:'+e.target.className);
-	console.log("Event Target 객체 classList.contains('guest_item_a'):"+e.target.classList.contains('guest_itme_a'));
+	console.log("Event Target 객체 classList.contains('guest_item_a'):"+e.target.classList.contains('guest_item_a'));
+	/****************guest_detail****************/
+	if(e.target.classList.contains('guest_item_a')){
+		/*
+		<<e.target>>
+		<a href="#" class="user guest_item_a" class="guest_item_a" guest_no="{{guest_no}}">
+		*/
+		let params ='guest_no='+e.target.getAttribute("guest_no");
+		
+		let jsonResult = Service.guestService('GET',URL.GUEST_DETAIL_URL,params);
+		View.render("#guest-detail-template",jsonResult,"#content");
+	}
+	/****************guest_delete_action****************/
+	if(e.target.id=='btn_guest_remove_action'){
+		let params ='guest_no='+e.target.getAttribute("guest_no");
+		let jsonResult = Service.guestService('POST',URL.GUEST_REMOVE_ACTION_URL,params);
+		if(jsonResult.code==1){
+			menuGuestList.click();
+		}else{
+			alert(jsonResult.msg);
+		}
+	}
+	
+	
+	e.preventDefault();
 	
 });
 
