8f498675c7a33e15e5fa85a7198d1e5759bd56cb
meltingong <hwk1210@naver.com>
2023-03-10 16:39:57 +0900

    ..

--- guest_javascript_ajax/WebContent/js/app.js
+++ guest_javascript_ajax/WebContent/js/app.js
@@ -4,7 +4,9 @@
 import * as View from "./view.js";
 //import {render} from "./view.js";
 //import {render as Render} from "./view.js";
-import * as Service from "./service.js"
+import * as Service from "./service.js";
+import * as URL from "./request-url.js";
+
 
 /*
 메뉴객체 이벤트등록
@@ -18,7 +20,7 @@ menuGuestHome.addEventListener('click',function(e){
 	e.preventDefault();
 });
 menuGuestList.addEventListener('click',function(e){
-	let jsonResult = Service.guestService('GET','guest/guest_list_json.jsp','');
+	let jsonResult = Service.guestService('GET',URL.GUEST_LIST_URL,'');
 	console.log(jsonResult);
 	View.render("#guest-list-template",jsonResult,"#content");
 	e.preventDefault();
@@ -32,4 +34,16 @@ menuGuestWriteForm.addEventListener('click',function(e){
 /*
 초기 로딩 시에 home anchor click event trigger
 */
-menuGuestHome.click();
+//menuGuestHome.click();
+document.addEventListener('click',function(e){
+	
+	console.log('Event객체:'+e);
+	console.log('Event Target 객체:'+e.target);
+	console.log('Event Target 객체 id:'+e.target.id);
+	console.log('Event Target 객체 id:'+e.target.getAttribute("id"));
+	console.log('Event Target 객체 className:'+e.target.className);
+	console.log("Event Target 객체 classList.contains('guest_item_a'):"+e.target.classList.contains('guest_itme_a'));
+	
+});
+
+
--- guest_javascript_ajax/src/com/itwill/guest/DataSource.java
+++ guest_javascript_ajax/src/com/itwill/guest/DataSource.java
@@ -13,8 +13,8 @@ public class DataSource {
 		/*******************************************/
 		String driverClass = "oracle.jdbc.OracleDriver";
 		String url = "jdbc:oracle:thin:@182.237.126.19:1521:xe";
-		String user = "jdeveloper30";
-		String password = "jdeveloper30";
+		String user = "jdeveloper04";
+		String password = "jdeveloper04";
 		/*******************************************/
 		Class.forName(driverClass);
 		Connection con = DriverManager.getConnection(url, user, password);
