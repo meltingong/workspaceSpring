28851c91b00c3d2ed8bc36616089b2bf8ce1d6d7
meltingong <hwk1210@naver.com>
2023-03-15 16:16:46 +0900

    ..

--- guest_spring_rest_jquery_ajax/src/main/resources/static/js/app.js
+++ guest_spring_rest_jquery_ajax/src/main/resources/static/js/app.js
@@ -1,3 +1,4 @@
+import * as View from "./view.js";
 /*
  메뉴객체 이벤트 등록 
 	#menu_guest_home
@@ -18,45 +19,51 @@
  /********guest_home*******/
 $('#menu_guest_home').click(function(e){
 	console.log(e.target);
+	View.render("#guest-main-template");
 	e.preventDefault();
 });
  /********guest_list*******/
 $(document).on('click','#menu_guest_list','#btn_guest_list',function(e){
 	console.log(e.target);
+	View.render("#guest-list-template",{});
 	e.preventDefault();
 });
  /********guest_write_form*******/
 $(document).on('click','#menu_guest_write_form','#btn_guest_write_form',function(e){
 	console.log(e.target);
+	View.render("#guest-write-form-template");
 	e.preventDefault();
 });
 
  /********guest_write_action*******/
-$(document).on('click','#btn_guest_write_action',function(){
+$(document).on('click','#btn_guest_write_action',function(e){
 	console.log(e.target);
+	
 	e.preventDefault();
 });
 
  /********guest_modify_form*******/
-$(document).on('click','#btn_guest_modify_form',function(){
+$(document).on('click','#btn_guest_modify_form',function(e){
 	console.log(e.target);
+	View.render("#guest-modify-form-template");
 	e.preventDefault();
 });
 
  /********guest_modify_action*******/
-$(document).on('click','#btn_guest_modify_action',function(){
+$(document).on('click','#btn_guest_modify_action',function(e){
 	console.log(e.target);
 	e.preventDefault();
 });
 
  /********guest_remove_action*******/
-$(document).on('click','#btn_guest_remove_action',function(){
+$(document).on('click','#btn_guest_remove_action',function(e){
 	console.log(e.target);
 	e.preventDefault();
 });
  /********guest_detail*******/
-$(document).on('click','.guest_item_a',function(){
+$(document).on('click','.guest_item_a',function(e){
 	console.log(e.target);
+	View.render("#guest-detail-template",{});
 	e.preventDefault();
 });
 
