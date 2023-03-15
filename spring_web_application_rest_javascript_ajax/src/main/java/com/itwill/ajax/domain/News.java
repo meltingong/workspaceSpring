83a7908eabe507d902e2be8c45a53ea87b27d799
meltingong <hwk1210@naver.com>
2023-03-15 11:25:18 +0900

    ..

--- spring_web_application_rest_javascript_ajax/src/main/java/com/itwill/ajax/domain/News.java
+++ spring_web_application_rest_javascript_ajax/src/main/java/com/itwill/ajax/domain/News.java
@@ -1,5 +1,7 @@
 package com.itwill.ajax.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +13,7 @@ import lombok.ToString;
 @ToString
 public class News {
 	private int no;
+	@JsonProperty(value="newsTitle")
 	private String title;
 	private String company;
 	private String date;
