b9e281b13fb107b0e5b00f5d3e160a603d984336
meltingong <hwk1210@naver.com>
2023-03-15 15:28:29 +0900

    ..

--- guest_spring_rest_jquery_ajax/src/main/resources/static/js/app.js
+++ guest_spring_rest_jquery_ajax/src/main/resources/static/js/app.js
@@ -2,4 +2,12 @@
  메뉴객체 이벤트 등록 
  */
  
- 
+$('#menu_guest_home').click(function(e){
+	console.log(e.target);
+});
+$('#menu_guest_list').click(function(e){
+	console.log(e.target);
+});
+$('#menu_guest_write_form').click(function(e){
+	console.log(e.target);
+});
