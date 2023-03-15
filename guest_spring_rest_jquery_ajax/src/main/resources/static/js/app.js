e7e104191751b03791d955a26b2ecef3c428ec8c
meltingong <hwk1210@naver.com>
2023-03-15 16:02:09 +0900

    ..

--- guest_spring_rest_jquery_ajax/src/main/resources/static/js/app.js
+++ guest_spring_rest_jquery_ajax/src/main/resources/static/js/app.js
@@ -15,39 +15,46 @@
 	
 	.guest_item_a
  */
- 
+ /********guest_home*******/
 $('#menu_guest_home').click(function(e){
 	console.log(e.target);
 	e.preventDefault();
 });
+ /********guest_list*******/
 $(document).on('click','#menu_guest_list','#btn_guest_list',function(e){
 	console.log(e.target);
 	e.preventDefault();
 });
+ /********guest_write_form*******/
 $(document).on('click','#menu_guest_write_form','#btn_guest_write_form',function(e){
 	console.log(e.target);
 	e.preventDefault();
 });
 
+ /********guest_write_action*******/
 $(document).on('click','#btn_guest_write_action',function(){
 	console.log(e.target);
 	e.preventDefault();
 });
 
+ /********guest_modify_form*******/
 $(document).on('click','#btn_guest_modify_form',function(){
 	console.log(e.target);
 	e.preventDefault();
 });
 
+ /********guest_modify_action*******/
 $(document).on('click','#btn_guest_modify_action',function(){
 	console.log(e.target);
 	e.preventDefault();
 });
 
+ /********guest_remove_action*******/
 $(document).on('click','#btn_guest_remove_action',function(){
 	console.log(e.target);
 	e.preventDefault();
 });
+ /********guest_detail*******/
 $(document).on('click','.guest_item_a',function(){
 	console.log(e.target);
 	e.preventDefault();
