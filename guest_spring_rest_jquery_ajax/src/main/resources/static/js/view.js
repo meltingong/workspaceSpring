d270428717d0808fe0f55b1f2606c7952e548339
meltingong <hwk1210@naver.com>
2023-03-15 16:03:24 +0900

    ..

--- guest_spring_rest_jquery_ajax/src/main/resources/static/js/view.js
+++ guest_spring_rest_jquery_ajax/src/main/resources/static/js/view.js
@@ -1,6 +1,9 @@
 /**
  * 
  */
- export function render(){
-	
+export function render(templateId='#guest-main-template',jsonResult={},contentId="#content") {
+	let templateHtml = $(templateId).html();
+	let bindTemplate = Handlebars.compile(templateHtml);
+	let resultTemplate = bindTemplate(jsonResult);
+	$(contentId).html(resultTemplate);
 }
