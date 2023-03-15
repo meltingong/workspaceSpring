bb88e4f723c3c08e5cc036d7bd4116d5b888dd59
meltingong <hwk1210@naver.com>
2023-03-15 17:48:45 +0900

    ..

--- guest_spring_rest_jquery_ajax/src/main/resources/static/js/app.js
+++ guest_spring_rest_jquery_ajax/src/main/resources/static/js/app.js
@@ -1,4 +1,5 @@
 import * as View from "./view.js";
+import * as Request from "./request.js";
 /*
  메뉴객체 이벤트 등록 
 	#menu_guest_home
@@ -25,22 +26,26 @@ $('#menu_guest_home').click(function(e){
  /********guest_list*******/
 $(document).on('click','#menu_guest_list','#btn_guest_list',function(e){
 	
-	let url='';
-	let jsonData={};
+	let url='guest';
 	let method = 'GET';
+	let contentType="application/json;charset=UTF-8";
+	let sendData={};
 	let async=true;
+	Request.ajaxRequest(url,
+						method,
+						contentType,
+						sendData,
+						function(resultJson){
+							View.render("#guest-list-template",resultJson);
+						},
+						async);	
 	
-	$.ajax({
-		url:url,
-		method:method,
-		data:jasonData,
-		success:function(){
-			
-		},
-		async:async
-	})
-	
-	View.render("#guest-list-template",{});
+	e.preventDefault();
+});
+ /********guest_detail*******/
+$(document).on('click','.guest_item_a',function(e){
+	console.log(e.target);
+	View.render("#guest-detail-template",{});
 	e.preventDefault();
 });
  /********guest_write_form*******/
@@ -52,7 +57,30 @@ $(document).on('click','#menu_guest_write_form','#btn_guest_write_form',function
 
  /********guest_write_action*******/
 $(document).on('click','#btn_guest_write_action',function(e){
-	console.log(e.target);
+	
+	let sendData={
+		guest_name:document.f.guest_name.value,
+		guest_email:document.f.guest_email.value,
+		guest_homepage:document.f.guest_homepage.value,
+		guest_title:document.f.guest_title.value,
+		guest_content:document.f.guest_content.value
+	};
+	console.log(sendData);
+	Request.ajaxRequest('guest',
+						'POST',
+						'application/json;charset=UTF-8',
+						JSON.stringify(sendData),
+						function(resultJson){
+							if(resultJson.code==1){
+								
+								
+								
+							}else{
+								alert(resultJson.msg);
+							}
+						},
+						true);
+	
 	e.preventDefault();
 });
 
@@ -72,12 +100,6 @@ $(document).on('click','#btn_guest_modify_action',function(e){
  /********guest_remove_action*******/
 $(document).on('click','#btn_guest_remove_action',function(e){
 	console.log(e.target);
-	e.preventDefault();
-});
- /********guest_detail*******/
-$(document).on('click','.guest_item_a',function(e){
-	console.log(e.target);
-	View.render("#guest-detail-template",{});
 	e.preventDefault();
 });
 
--- guest_spring_rest_jquery_ajax/src/main/resources/static/js/view.js
+++ guest_spring_rest_jquery_ajax/src/main/resources/static/js/view.js
@@ -4,6 +4,11 @@
 export function render(templateId='#guest-main-template',jsonResult={},contentId="#content") {
 	let templateHtml = $(templateId).html();
 	let bindTemplate = Handlebars.compile(templateHtml);
+	/****************Handlebars함수등록****************/
+	Handlebars.registerHelper('substring',function(str,start,end){
+		return str.substring(start,end);
+	});
+	
 	let resultTemplate = bindTemplate(jsonResult);
 	$(contentId).html(resultTemplate);
 }
