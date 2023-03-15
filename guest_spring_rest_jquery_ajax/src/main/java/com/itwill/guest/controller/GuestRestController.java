d6819a8c59b1eadad9c2b3777ca6bf3e7b4ef705
meltingong <hwk1210@naver.com>
2023-03-15 11:49:27 +0900

    ..

---
+++ guest_spring_rest_jquery_ajax/src/main/java/com/itwill/guest/controller/GuestRestController.java
@@ -0,0 +1,11 @@
+package com.itwill.guest.controller;
+	/*
+	1. GET    - /guest : 모든 데이터를 요청한다.
+	2. POST   - /guest : 데이터를 추가한다.
+	3. GET    - /guest/n : n번째 데이터를 요청한다.
+	4. PUT    - /guest/n : n번째 데이터를 수정한다.
+	5. DELETE - /guest/n : n번째 데이터를 제거한다.
+	*/
+public class GuestRestController {
+
+}
