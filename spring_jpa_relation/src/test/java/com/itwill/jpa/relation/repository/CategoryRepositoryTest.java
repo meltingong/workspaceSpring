4e6c19c207fd37b9530f75e366fd329d794a5bd4
meltingong <hwk1210@naver.com>
2023-03-31 11:02:27 +0900

    ..

---
+++ spring_jpa_relation/src/test/java/com/itwill/jpa/relation/repository/CategoryRepositoryTest.java
@@ -0,0 +1,13 @@
+package com.itwill.jpa.relation.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.itwill.jpa.relation.SpringJpaRelationApplicationTests;
+
+class CategoryRepositoryTest extends SpringJpaRelationApplicationTests{
+
+	
+
+}
