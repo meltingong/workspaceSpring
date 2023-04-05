3b149ff39684f4cd01430ee0e11c5f2ec4aec8cd
meltingong <hwk1210@naver.com>
2023-04-05 11:22:46 +0900

    ..

--- spring_security_form_login1/src/main/java/com/itwill/security/config/SecurityConfig.java
+++ spring_security_form_login1/src/main/java/com/itwill/security/config/SecurityConfig.java
@@ -1,9 +1,25 @@
 package com.itwill.security.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity(debug = true)
-public class SecurityConfig {
-	UsernamePasswordAuthenticationFilter filter;
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser(User.builder().username("user2").password(passwordEncoder().encode("2222")).roles("USER"))
+			.withUser(User.builder().username("admin").password("3333").roles("ADMIN"));
+	}
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 }
