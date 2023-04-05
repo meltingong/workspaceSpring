737c569309d1077a5844e97c9ce01e763fb96309
meltingong <hwk1210@naver.com>
2023-04-05 11:26:25 +0900

    ..

--- spring_security_form_login1/src/main/java/com/itwill/security/config/SecurityConfig.java
+++ spring_security_form_login1/src/main/java/com/itwill/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.itwill.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -20,6 +21,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests().anyRequest().permitAll();
 	}
 	
 }
