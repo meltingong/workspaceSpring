286aa3b735bfabdcc093e560906ec5769da20465
meltingong <hwk1210@naver.com>
2023-04-06 09:45:03 +0900

    ..

--- spring_security_form_login1/src/main/java/com/itwill/security/controller/HomeController2.java
+++ spring_security_form_login1/src/main/java/com/itwill/security/controller/HomeController2.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class HomeController2 {
 	@GetMapping("/")
 	public String main() {
 		return "index";
--- spring_security_form_login1/src/main/java/com/itwill/security/controller/HomeRestController1.java
+++ spring_security_form_login1/src/main/java/com/itwill/security/controller/HomeRestController1.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //@RestController
-public class HomeRestController {
+public class HomeRestController1 {
 	@GetMapping("/")
 	public String index() {
 		return "홈페이지";
