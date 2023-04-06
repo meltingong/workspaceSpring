ad4a09a5ce65a2fa9c0a1b444c155458317df9c7
meltingong <hwk1210@naver.com>
2023-04-06 10:38:10 +0900

    ..

--- spring_security_form_login1/src/main/java/com/itwill/security/config/SecurityConfig2.java
+++ spring_security_form_login1/src/main/java/com/itwill/security/config/SecurityConfig2.java
@@ -2,6 +2,8 @@ package com.itwill.security.config;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -46,6 +48,14 @@ public class SecurityConfig2 extends WebSecurityConfigurerAdapter{
 		
 		//http.formLogin().disable().httpBasic().disable();
 	}
+	@Bean
+	RoleHierarchy roleHierarchy() {
+		RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+		roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+		return roleHierarchy;
+	}
+	// admin 은 user 페이지, admin 페이지 접근 가능 / user 는 user페이지만 접근 가능
+	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
