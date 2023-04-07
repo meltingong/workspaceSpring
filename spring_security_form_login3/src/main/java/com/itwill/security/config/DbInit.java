e944abc4801915f1a4728780230055329e65d512
meltingong <hwk1210@naver.com>
2023-04-07 09:54:20 +0900

    ..

--- spring_security_form_login3/src/main/java/com/itwill/security/config/DbInit.java
+++ spring_security_form_login3/src/main/java/com/itwill/security/config/DbInit.java
@@ -15,14 +15,16 @@ public class DbInit implements InitializingBean {
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		if (!userService.findUser("user@test.com").isPresent()) {
+		if (!userService.findUser("user1").isPresent()) {
 			Users user1 = userService
-					.save(Users.builder().email("user1@test.com").password("1111").enabled(true).build());
-			
-			Users user2 = userService
-					.save(Users.builder().email("user2@test.com").password("2222").enabled(true).build());
-			
+					.save(Users.builder().email("user1").password("1111").enabled(true).build());
 			userService.addAuthority(user1.getUserId(), "ROLE_USER");
+		}
+		if (!userService.findUser("user2").isPresent()) {
+			Users user2 = userService
+					.save(Users.builder().email("user2").password("2222").enabled(true).build());
+			
+			userService.addAuthority(user2.getUserId(), "ROLE_USER");
 			userService.addAuthority(user2.getUserId(), "ROLE_ADMIN");
 		}
 
