ecc184092ffbae0ae0e22407d51c9d3c5eee09b6
meltingong <hwk1210@naver.com>
2023-04-10 22:02:16 +0900

    ,,

--- final-project-practice/src/main/java/com/itwill/ilhajob/config/WebConfig.java
+++ final-project-practice/src/main/java/com/itwill/ilhajob/config/WebConfig.java
@@ -2,8 +2,6 @@ package com.itwill.ilhajob.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.itwill.ilhajob.user.controller.AuthLoginAnnotationInterceptor;
